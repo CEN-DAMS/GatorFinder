347c51d724f2c30faa56e06055e2d9253c8eb972
ansh-kalariya <92905580+ansh-kalariya@users.noreply.github.com>
2025-03-30 20:08:08 -0400

    moved unit tests to one file

--- GatorFinder/Frontend/gatorfinder-frontend/src/App.test.js
+++ GatorFinder/Frontend/gatorfinder-frontend/src/App.test.js
@@ -1,3 +1,4 @@
+import React from 'react';
 import { render, screen, fireEvent } from '@testing-library/react';
 import App from './App';
 
@@ -15,4 +16,22 @@ test('search bar functionality test', () => {
   expect(event1).toBeInTheDocument();
   const otherEvents = screen.queryByText(/event 2/i);
   expect(otherEvents).not.toBeInTheDocument();
+});
+
+describe('Navigation between Login and Signup pages', () => {
+  test('navigates from Login to Signup page', () => {
+    window.history.pushState({}, 'Login Page', '/login');
+    render(<App />);
+    expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument();
+    fireEvent.click(screen.getByRole('link', { name: /sign up/i }));
+    expect(screen.getByRole('heading', { name: /sign up/i })).toBeInTheDocument();
+  });
+
+  test('navigates from Signup to Login page', () => {
+    window.history.pushState({}, 'Sign Up Page', '/signup');
+    render(<App />);
+    expect(screen.getByRole('heading', { name: /sign up/i })).toBeInTheDocument();
+    fireEvent.click(screen.getByRole('link', { name: /login/i }));
+    expect(screen.getByRole('heading', { name: /login/i })).toBeInTheDocument();
+  });
 });
